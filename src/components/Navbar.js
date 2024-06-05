45619df7cf73dc02c7bcfba280b2bd6df9effb0e
Chris <cpena3@buffalo.edu>
2024-06-04 20:57:39 -0400

    Navbar created

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import AirbnbLogo from "../images/airbnb-logo.png"
+
+
+function Navbar(){
+    return(
+        <nav>
+            <img src={AirbnbLogo} alt="Logo"/>
+        </nav>
+    )
+}
+
+export default Navbar
