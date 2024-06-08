c0d5e5f2ef85ff615761e96b6e8870e34181f6dd
Chris <cpena3@buffalo.edu>
2024-06-07 23:46:00 -0400

    Changed font style

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,6 +3,7 @@ import Image from '../images/photo-grid.png'
 function Hero(){
     return(
         <div>
+            <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;600&display=swap" rel="stylesheet"></link>
             <img src={Image} alt="heroLogo" className="hero--logo"/>
             <h1>Online Experiences</h1>
             <p>Join unique interactive actvities led by one-of-a-kind hosts-all without leaving home.</p>
