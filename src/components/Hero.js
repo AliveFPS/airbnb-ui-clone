0fde7751bb6a0d68d5e89438bd51acde1de87eb6
Chris <cpena3@buffalo.edu>
2024-06-07 23:51:26 -0400

    Styling Changes to the hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,12 +2,12 @@ import Image from '../images/photo-grid.png'
 
 function Hero(){
     return(
-        <div>
+        <section className="hero">
             <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;600&display=swap" rel="stylesheet"></link>
-            <img src={Image} alt="heroLogo" className="hero--logo"/>
-            <h1>Online Experiences</h1>
-            <p>Join unique interactive actvities led by one-of-a-kind hosts-all without leaving home.</p>
-        </div>
+            <img src={Image} alt="heroLogo" className="hero--photo"/>
+            <h1 className="hero--header">Online Experiences</h1>
+            <p className="hero--text">Join unique interactive actvities led by one-of-a-kind hosts-all without leaving home.</p>
+        </section>
     )
 }
 
